43d9ddc6198088a01b0d3664d484c049ef00a894
duguowei666 <381335863@qq.com>
2020-07-22 08:49:20 +0800

    duguowei

---
+++ resources/views/admin/area/list.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap 实例</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    省:<select name="province" id="" class="changearea">
+        <option value="" selected="selected">--请选择--</option>
+        @foreach($data as $v)
+        <option value="{{$v['id']}}" >{{$v['name']}}</option>
+        @endforeach
+    </select>
+    城市:
+    <select name="city" id="city">
+        <option value="" selected="selected">--请选择--</option>
+    </select>
+    区
+    <select name="area" id="crea">
+        <option value="" selected="selected">--请选择--</option>
+    </select>
+</div>
+
+</body>
+</html>
+<script>
+    $(document).on('change','.changearea',function () {
+       var id = $(this).val();
+       var url = '/admins/getcity';
+        $.ajax({
+            url:url,
+            data:{'id':id},
+            dataType:'json',
+            type: "post",
+            success:function (res) {
+                $("select[name='city']").empty();
+                $.each(res,function(k,v){
+                    var _option = "";
+                    _option += "<option value='"+v.id+"'>"+v.name+"</option>";
+                    $("select[name='city']").append(_option);
+                });
+            }
+        })
+    })
+    $(document).on('change','#city',function () {
+        var id = $(this).val();
+        var url = '/admins/getarea';
+        $.ajax({
+            url:url,
+            data:{'id':id},
+            dataType:'json',
+            type: "post",
+            success:function (res) {
+                $("select[name='area']").empty();
+                $.each(res,function (k,v) {
+                    var _option = "";
+                    _option += "<option value='"+v.id+"'>"+v.name+"</option>";
+                    $("select[name='area']").append(_option);
+                })
+            }
+        })
+    })
+</script>
--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin', function () {
+Route::get('/admins', function () {
     return view('admin.index');
 });
 Route::get('/admin_home', function () {
     return view('admin.home');
 });
-Route::prefix('admin')->group(function (){
+
+Route::prefix('admin')->group(function () {
     Route::get('pipe_add', function () {
         return view('admin.pipe.pipe_add');
     });
-    Route::any("pipe_log",function (){
+    Route::any("pipe_log", function () {
         return view('admin.pipe.pipe_log');
     });
-    Route::any('pipe_adds','PipeController@pipe_adds');
-    Route::any('pipe_zhan','PipeController@pipe_zhan');
-    Route::any('pipe_zhan','PipeController@pipe_');
+    Route::any('pipe_adds', 'PipeController@pipe_adds');
+    Route::any('pipe_zhan', 'PipeController@pipe_zhan');
+    Route::any('pipe_zhan', 'PipeController@pipe_');
+});
+Route::prefix('admins')->group(function () {
+    //商品
+    Route::get('goods','Admin\GoodsController@goodsadd');
+    Route::post('do_goodsadd','Admin\GoodsController@do_goodsadd');
+    Route::get('goodslist','Admin\GoodsController@goodslist');
+    Route::get('delgoods','Admin\GoodsController@delgoods');
+    Route::get('/upgoods/{id}','Admin\GoodsController@upgoods');
+    Route::post('/do_upgoods','Admin\GoodsController@do_upgoods');
+    //三级联动
+    Route::get('area','Admin\AreaController@area');
+    Route::any('getcity','Admin\AreaController@city');
+    Route::any('getarea','Admin\AreaController@getarea');
+
+
+    //===============================三级联动 ===========================
+
 });
