ba44adf63bdf49c758444f49e8b3e9c5f2ff0ef9
duguowei666 <381335863@qq.com>
2020-07-27 08:38:06 +0800

    goods

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Index;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-
+use Illuminate\Support\Facades\DB;
 class IndexController extends Controller
 {
     //
     public function index(){
-        return view('index.index');
+        $goods_info = DB::table('goods')->leftjoin('shop_category','goods.cate_id','=','shop_category.cate_id')->get();
+
+        return view('index.index',['goods_info'=>$goods_info]);
     }
 }
